07fa605f276835b940c3c5c0f91c20c12c93b587
sawergon <sasha.sesyukalov@gmail.com>
2023-09-27 10:46:52 +0300

    сделал вторую лабу

---
+++ phoneBook/phoneBook/phoneBook/Book.cs
@@ -0,0 +1,86 @@
+﻿namespace phoneBook
+{
+    public class Person
+    {
+        private string name, surName, secondName;
+        private int phone;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string SurName
+        {
+            get { return surName; }
+            set { surName = value; }
+        }
+
+        public string SecondName
+        {
+            get { return secondName; }
+            set { secondName = value; }
+        }
+
+        public int Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        public Person(string name, string surname, string secondName, int phone)
+        {
+            this.name = name;
+            surName = surname;
+            this.secondName = secondName;
+            this.phone = phone;
+        }
+
+        public void display()
+        {
+            Console.WriteLine(name + " " + surName + " " + secondName + " " + phone);
+        }
+    }
+
+    public class Book
+    {
+        private Person[] data;
+        private int capacity, len;
+
+        public Book(Person newPerson)
+        {
+            capacity = len = 1;
+            data = new Person [capacity];
+            data[0] = newPerson;
+        }
+
+        public void addNewPerson(Person newPerson)
+        {
+            if (len >= capacity)
+            {
+                capacity *= 2;
+                Person [] newData = new Person[len];
+                for (int i = 0; i < len; ++i)
+                {
+                    newData[i] = data[i];
+                }
+
+                data = new Person [capacity];
+                for (int i = 0; i < len; i++)
+                {
+                    data[i] = newData[i];
+                }
+            }
+            data[len++] = newPerson;
+        }
+
+        public void display()
+        {
+            for (int i = 0; i < capacity; ++i)
+            {
+                data[i].display();
+            }
+        }
+    }
+}
---
+++ phoneBook/phoneBook/phoneBook/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace phoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person a = new Person("a", "b", "c", 53);
+            Person ab = new Person("ab", "bd", "cf", 55);
+            // a.display();
+            Book b = new Book(a);
+            b.display();
+            b.addNewPerson(ab);
+            b.display();
+        }
+    }
+}
