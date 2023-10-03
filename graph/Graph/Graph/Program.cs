08a23be64c8d31806e9ef6e345c6463203b4a91f
sawergon <sasha.sesyukalov@gmail.com>
2023-10-03 10:17:11 +0300

    Сделал основу для графа

---
+++ graph/Graph/Graph/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graph
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var s = new Stack();
+            s.Push(5);
+            s.Push(6);
+            s.Push(7);
+            Console.WriteLine(s.Pop());
+            Console.WriteLine(s.Pop());
+            Console.WriteLine(s.Pop());
+            // Application.EnableVisualStyles();
+            // Application.SetCompatibleTextRenderingDefault(false);
+            // Application.Run(new Form1());
+        }
+    }
+}
---
+++ graph/Graph/Graph/Stack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Graph
+{
+    public class Stack
+    {
+        private int[] _data;
+        private int _len, _capacity;
+
+        public Stack()
+        {
+            _len = 0;
+            _capacity = 1;
+            _data = new int[_capacity];
+        }
+
+        public void Push(int value)
+        {
+            _data[_len++] = value;
+            if (_len < _capacity) return;
+            _capacity *= 2;
+            Array.Resize(ref _data, _capacity);
+        }
+
+        public bool IsEmpty()
+        {
+            return _len == 0;
+        }
+
+        public int Size()
+        {
+            return _len;
+        }
+
+        public int Pop()
+        {
+            return _data[--_len];
+        }
+
+        public int Top()
+        {
+            return _data[_len - 1];
+        }
+    }
+}
